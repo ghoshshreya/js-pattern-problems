51f1e566049096abb1dad704e91a8ec0f69c4487
ghoshshreya <shrghosh08@gmail.com>
2023-03-07 16:25:45 +0530

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import './style.css';
 import './patternProblems/1_pattern.js';
 import './patternProblems/2_pattern.js';
 import './patternProblems/3_pattern.js';
+import './patternProblems/4_pattern.js';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
---
+++ patternProblems/4_pattern.js
@@ -0,0 +1,15 @@
+// var getPattern = function (N) {
+//   for (let x = 1; x < N; x++) {
+//     // let str = '';
+//     // for (let y = 1; y <= x; x++) {
+//     //   str = str + x.toString();
+//     // }
+//     // console.log('Hello', str);
+//   }
+// };
+
+var getPattern = function (N) {
+  console.log(N);
+};
+
+getPattern(5);
