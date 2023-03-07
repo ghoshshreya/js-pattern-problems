832bb5e84e8f02e0a1a0461d752a4c275c4ba962
ghoshshreya <shrghosh08@gmail.com>
2023-03-07 16:27:11 +0530

    1 file modified

--- patternProblems/4_pattern.js
+++ patternProblems/4_pattern.js
@@ -1,15 +1,18 @@
-// var getPattern = function (N) {
-//   for (let x = 1; x < N; x++) {
-//     // let str = '';
-//     // for (let y = 1; y <= x; x++) {
-//     //   str = str + x.toString();
-//     // }
-//     // console.log('Hello', str);
-//   }
-// };
+// N = 5
+// 1
+// 22
+// 333
+// 4444
+// 55555
 
 var getPattern = function (N) {
-  console.log(N);
+  for (let x = 1; x <= N; x++) {
+    let str = '';
+    for (let j = 1; j <= x; j++) {
+      str = str + x.toString();
+    }
+    console.log(str);
+  }
 };
 
 getPattern(5);
